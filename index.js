10d20724f70b99c23d3bb00d865796790d140136
Joshua McClure <joshua.mcclure@deque.com>
2019-10-24 09:33:38 -0400

    Adds an invisible div layer which will block all mouse interactions with the page

--- index.js
+++ index.js
@@ -16,8 +16,28 @@ function addStyle(styles) {
     /* Append style to the tag name */ 
     document.getElementsByTagName("head")[0].appendChild(css); 
 }
+
+/* Function to add an overlay which will block all mouse interaction with the page */
+function addOverlay() {
+    // create new div element
+    var overlay = document.createElement('div');
+    
+    // element styles - sets an invisible layer on top of the page.
+    overlay.setAttribute('style', `
+        position: fixed;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: 999999;
+    `);
+
+    // append element to the html body
+    document.body.appendChild(overlay);
+}
     
 /* Function call */
 window.onload = function() {
-    addStyle(styles)
+    addStyle(styles);
+    addOverlay();
 }
